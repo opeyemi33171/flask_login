b4c7ad64b243bcd21ed6a23055a870bff98d14a9
opeyemi33171 <opeyemi33171@gmail.com>
2015-12-13 19:42:28 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome_page():
+    return "Welcome god4saken"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'opey' and request.form['password'] == 'answer':
+            error = 'You have entered the wrong username, password combination.'
+        else:
+            return redirect(url_for('welcome_page'))
+    return render_template('Login.html', error=error)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
